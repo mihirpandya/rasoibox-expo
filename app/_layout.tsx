e98646ea03a8e6a7a3f3f5c4e31bcffe6bdb46ec
Mihir Pandya <mihir.m.pandya@gmail.com>
2023-09-08 12:02:41 -0700

    header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,9 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import { useColorScheme, Image, View, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from '../components/Themed';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -21,6 +23,8 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    CormorantGaramondSemiBold: require('../assets/fonts/CormorantGaramond-SemiBold.ttf'),
+    AvenirLight: require('../assets/fonts/Avenir-Light.ttf'),
     ...FontAwesome.font,
   });
 
@@ -42,20 +46,58 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
+function LogoTitle() {
+  return (
+    <View style={{paddingLeft: '20%'}}>
+      <Image
+        style={styles.logo}
+        source={{ uri: '../assets/images/header_logo.svg' }}
+      />
+    </View>
+  );
+}
+
+function MenuOptions() {
+  return (
+    <View style={styles.menuOptions}>
+      <Text style={styles.menuItem}>Menu</Text>
+      <Text style={styles.menuItem}>About Us</Text>
+    </View>
+  )
+}
+
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f17a7e'
-          }
-        }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: 'white',
+        },
+      }}>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerTitle: _props => <LogoTitle />,
+          headerRight: _props => <MenuOptions />
+        }}
+      />
+    </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 225,
+    height: 50
+  },
+  menuOptions: {
+    paddingRight: '10%',
+  },
+  menuItem: {
+    padding: 20,
+    fontFamily: 'AvenirLight',
+    fontSize: 15,
+  }
+});
