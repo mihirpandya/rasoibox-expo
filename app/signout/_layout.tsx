db574c5da20ccec309b91ddaccae90eb7b6fc7c2
Mihir Pandya <mihir.m.pandya@gmail.com>
2023-09-12 10:11:41 -0700

    clean up

--- app/signout/_layout.tsx
+++ app/signout/_layout.tsx
@@ -1,14 +1,11 @@
 import { Redirect } from 'expo-router';
 import React from 'react';
-import { View } from 'react-native';
 import * as Storage from "../../components/common/Storage";
 
 export default function SignOut() {
     Storage.removeAuthDetails();
 
     return (
-        <View>
-            <Redirect href="/" />
-        </View>
+        <Redirect href="/" />
     );
 }
