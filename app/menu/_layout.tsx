1cb536bc369bf900f7e639071052e5ae555399ac
Mihir Pandya <mihir.m.pandya@gmail.com>
2023-09-12 13:07:39 -0700

    menu

--- app/menu/_layout.tsx
+++ app/menu/_layout.tsx
@@ -1,32 +1,82 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import { Text } from '../../components/Themed';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Footer from '../../components/common/Footer';
 import Header from '../../components/common/Header';
+import MealKitCard from "../../components/menu/MealKitCard";
 import { getAvailableItems } from '../api/rasoibox-backend';
 
+interface MealKit {
+    id: number,
+    name: string,
+    description: string,
+    imageUrl: string,
+    servingSizes: number[],
+    prices: number[],
+    cookTime: number,
+    prepTime: number,
+    tags: string[]
+}
+
 export default function Menu() {
-    const [availableItems, setAvailableItems] = useState()
+    const [availableItems, setAvailableItems] = useState<MealKit[]>([])
 
     const fetchAvailableItems = () => {
         getAvailableItems().then(response => {
-            setAvailableItems(response)
+            const keys = Object.keys(response);
+            const values = Object.values(response);
+            const items: MealKit[] = []
+            for (let i = 0; i < keys.length; i++) {
+                items.push({
+                    id: keys[i],
+                    name: values[i]['recipe_name'],
+                    description: values[i]['description'],
+                    imageUrl: values[i]['image_url'],
+                    servingSizes: values[i]['serving_sizes'],
+                    prices: values[i]['prices'],
+                    cookTime: values[i]['cook_time'],
+                    prepTime: values[i]['prep_time'],
+                    tags: values[i]['tags']
+                })
+            }
+            setAvailableItems(items)
         })
     }
 
     useEffect(() => {
         fetchAvailableItems()
-      }, [])
+      }, []);
     
     return (
-        <View>
-            <Header />
-
-            <Text>
-                {JSON.stringify(availableItems)}
-            </Text>
-
-            <Footer />
+        <View style={{flex: 1}}>
+            <ScrollView>
+                <Header />
+                    <View style={styles.card}>
+                        {availableItems.map(item => <MealKitCard 
+                            id={item.id}
+                            name={item.name}
+                            description={item.description}
+                            imageUrl={item.imageUrl}
+                            servingSizes={item.servingSizes}
+                            prices={item.prices}
+                            cookTime={item.cookTime}
+                            prepTime={item.prepTime}
+                            tags={item.tags}
+                        />)}
+                    </View>
+                <Footer />
+            </ScrollView>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    card: {
+        paddingLeft: '10%',
+        paddingRight: '10%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItem: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+    }
+})
---
+++ components/menu/MealKitCard.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { Card } from 'react-native-paper';
+import { rasoiBoxYellow } from '../../constants/Colors';
+
+type MealKitCardProps = {
+    id: number,
+    name: string,
+    description: string,
+    imageUrl: string,
+    servingSizes: number[],
+    prices: number[],
+    cookTime: number,
+    prepTime: number,
+    tags: string[]
+}
+
+export default function MealKitCard(props: MealKitCardProps) {
+    const {
+        id,
+        name,
+        description,
+        imageUrl,
+        servingSizes,
+        prices,
+        cookTime,
+        prepTime,
+        tags
+    } = props;
+
+    return (
+        <Pressable>
+            <Card style={styles.card}>
+                <Card.Cover style={styles.cover} source={{uri: imageUrl}} />
+                <Card.Title titleStyle={styles.title} title={name}/>
+                <Card.Content>
+                    <Text style={styles.description}>
+                        {description}
+                    </Text>
+                    <View style={styles.tags}>
+                        {tags.map(tag => <Text style={styles.tag}>{tag}</Text>)}
+                    </View>
+                </Card.Content>
+            </Card>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+        width: 270,
+        height: 370,
+        backgroundColor: 'white',
+        margin: 20
+    },
+    cover: {
+        borderRadius: 0,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10
+    },
+    title: {
+        color: 'black',
+        fontFamily: 'AvenirLight',
+        fontSize: 20,
+    },
+    description: {
+        fontFamily: 'AvenirLight',
+        fontSize: 16,
+        color: 'black'
+    },
+    tags: {
+        flexDirection: 'row',
+        position: 'absolute',
+        marginTop: 95
+    },
+    tag: {
+        fontSize: 12,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 2,
+        paddingBottom: 2,
+        marginRight: 10,
+        fontFamily: 'AvenirLight',
+        backgroundColor: rasoiBoxYellow,
+        borderRadius: 10,
+        color: 'white'
+    }
+})
