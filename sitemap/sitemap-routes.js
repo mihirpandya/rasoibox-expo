cc8b10f3328b00929ef7b4111e9f28a00c2cfd48
Mihir Pandya <mihir.m.pandya@gmail.com>
2023-10-09 12:15:31 -0700

    basic sitemap
    Empty-Commit
    
    change vercel build command

---
+++ sitemap/sitemap-routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router';
+ 
+export default (
+    <Route>
+        <Route path='/' />
+        <Route path='/menu' />
+        <Route path='/our-story' />
+        <Route path='/startcooking' />
+        <Route path='/order-history' />
+    </Route>
+);
